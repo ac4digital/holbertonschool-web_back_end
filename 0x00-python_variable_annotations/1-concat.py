a6c25a811b25c317cbed42b1a7e2f4be25c88c0d
Andres Catano <acatano@ac4digital.com>
2022-09-19 17:21:59 -0500

    task 1 concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Script that concatenates two strings and 
+    returns a new string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings to create a new string
+    Args:
+        str1: string to concat
+        str2: string to concat
+    Returns: string, new string concatened by str1 and str2
+    """
+    return str1 + str2
