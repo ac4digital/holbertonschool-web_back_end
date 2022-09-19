bd69d7fca3859e128c4f5527cf67f1da71d78630
Andres Catano <acatano@ac4digital.com>
2022-09-19 17:09:51 -0500

    task 0 sum two floats first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Script that calculates the sum of
+two floats and returns a float """
+
+def add(a: float, b: float) -> float:
+    """ Calculates the sum of two floats
+    Args:
+        a: float to sum
+        b: float to sum
+    Returns: float, sum of a and b
+    """
+    return a + b
