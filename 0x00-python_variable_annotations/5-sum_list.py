8309754310b7bcf8327e78d02ad39e0e4f64ea12
Andres Catano <acatano@ac4digital.com>
2022-09-19 21:39:03 -0500

    task 5 takes a list of floats and returns their sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Script takes a list input_list of floats as
+    argument and returns their sum as a float
+"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """ Returns the sum of all the elements of a list
+    """
+    return sum(input_list)
