26a40b2b84446521c87ab8fab4793e8b5528e5db
Andres Catano <acatano@ac4digital.com>
2022-09-19 21:16:56 -0500

    task 3 takes a float number a returns the string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Script that takes a float number and
+    returns the string representation of the float """
+
+
+def to_str(n: float) -> str:
+    """ Cast a float number to string type
+    Args:
+        n: float
+    Returns: string
+    """
+    return str(n)
