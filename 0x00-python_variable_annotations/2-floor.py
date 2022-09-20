167f0d15fb7d1edbaf349b0296957f33a193e1dd
Andres Catano <acatano@ac4digital.com>
2022-09-19 21:13:12 -0500

    task 2 takes the floor of a float number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Script that takes a float number and
+    returns the floor of the float """
+
+
+def floor(n: float) -> int:
+    """ Cast a float number to int number
+    Args:
+        n: float
+    Returns: int
+    """
+    return int(n)
