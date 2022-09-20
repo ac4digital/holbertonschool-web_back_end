79b278449e081ce66e12a99b7898c3d472c1fa2a
Andres Catano <acatano@ac4digital.com>
2022-09-19 21:28:54 -0500

    task 4 define and annotate variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Define and annotate the following variables
+    with the specific values 
+        a: int
+        pi: float
+        i_understand_annotations: boolean
+        school: Holberton 
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
