f5bef3b22562fdb79fe7ee76cf864a65863b230d
Andres Catano <acatano@ac4digital.com>
2022-09-21 09:23:02 -0500

    task 4 take a code and alter it into a new function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -12,9 +12,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Returns a list of delayed float values
         using wait_random coroutine """
-    tasks = []
+    tasks: List[float] = []
 
     for _ in range(n):
         tasks.append(task_wait_random(max_delay))
 
-    return [await t for t in asyncio.as_completed(tasks)]
+    return [await process for process in asyncio.as_completed(tasks)]
