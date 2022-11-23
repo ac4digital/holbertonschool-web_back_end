414f31e09f5623462f81c136e0aebd0e6d94d54a
Andres Catano <acatano@ac4digital.com>
2022-11-22 21:27:27 -0500

    First Commit

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,6 +1,6 @@
 const weakMap = new WeakMap();
 
-const queryAPI _= (endpoint) => {
+const queryAPI = (endpoint) => {
     if (weakMap.has(endpoint)) {
         weakMap.set(endpoint, weakMap.get(endpoint) + 1);
         if (weakMap.get(endpoint) >= 5) {
