a178c89f8b91b0f4c9c7103e9c9032599c6fdee7
Andres Catano <acatano@ac4digital.com>
2022-11-22 23:05:20 -0500

    First Commit

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,9 @@
-// returns a new ArrayBuffer with an Int8 value at a specific position.
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
-  const array = new Int8Array(buffer, 0, length);
-  if (position >= 0 && position < length) array[position] = value;
-  else throw Error('Position outside range');
-  return buffer;
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 }
-
-/* console.log(createInt8TypedArray(10, 2, 89));
-createInt8TypedArray(10, 12, 89); */
