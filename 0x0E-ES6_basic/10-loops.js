f220c9510293bb13168d5d9a60781f161e1496c8
Andres Catano <acatano@ac4digital.com>
2022-11-23 21:26:41 -0500

    First Commit

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-function appendToEachArrayValue(array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     array[array.indexOf(value)] = appendString + value;
   }
