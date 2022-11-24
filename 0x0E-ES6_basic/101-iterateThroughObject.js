556e27ca740e5918e14d28af420b8c1083881e5a
Andres Catano <acatano@ac4digital.com>
2022-11-23 21:43:39 -0500

    First Commit

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,15 @@
-export default function iterateThroughObject(reportWithIterator) {
-  if (!reportWithIterator) {
-    console.log('Placeholder');
+export default function createIteratorObject(report) {
+    let iterable = [];
+  
+    iterable = {
+      * [Symbol.iterator]() {
+        for (const value of Object.values(report.allEmployees)) {
+          for (const i of value) {
+            yield i;
+          }
+        }
+      },
+    };
+  
+    return iterable;
   }
-}
