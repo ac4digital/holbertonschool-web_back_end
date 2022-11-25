b73e7192e4932f5bde439b181be9904cce7a9c12
Andres Catano <acatano@ac4digital.com>
2022-11-24 19:16:15 -0500

    First Commit

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Task 11"""
+import pymongo
+
+
 def schools_by_topic(mongo_collection, topic):
-    """Returns the list of school having specific topic
     """
-    return (mongo_collection.find({"topics": topic}))
+    Returns the list of school having specific topic
+    """
+    return (mongo_collection.find({ "topics": topic }))
