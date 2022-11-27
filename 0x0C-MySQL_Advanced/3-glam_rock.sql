064f51b981021c38f63a0410d588a3b5565bcd4e
Andres Catano <acatano@ac4digital.com>
2022-11-27 15:30:06 -0500

    First Commit

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- Lists all band with Glam as their main style, ranked by
 -- their longetivity
-SELECT band_name, COALESCE(split, 2020) - formed AS 'lifespan'
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS 'lifespan'
 FROM metal_bands WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
